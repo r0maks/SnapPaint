f4aed3382db292b6b1f2cb645d7f107d04dcdc76
Roman Maksimov <rrmaksimov@gmail.com>
2019-11-06 08:33:13 -0500

    figuring out how to draw. Need smoother easing functionality

--- app.js
+++ app.js
@@ -1,34 +1,81 @@
 
+var isDragging = false;
+var color = '#000';
+var mousePositions = [];
+var elements = [];
+var strokeWidth = 20;
+var easing = 0.05;
+var x = 0;
+var y = 0;
+var px = 0;
+var py = 0;
+
+
 window.onload = function () {
-    var mousePositions = [];
-    var trailLimit = 100;
-    var enableDraw = false;
-    var circles = [];
-    var canvas = Snap(800, 800);
+
+    var canvas = Snap('#snapContainer');
 
     canvas.mousemove((event, x, y) => {
-        // mousePositions.push({ x, y })
-        if (enableDraw) {
-            addCircle(x, y);
+        mousePositions.push({ x, y });
+        if (isDragging) {
+            addLine(x, y);
         }
-        console.log(event, x, y);
     });
 
     canvas.drag(() => { }, () => {
-        enableDraw = true;
+        isDragging = true;
     }, () => {
-        enableDraw = false;
+        isDragging = false;
     });
 
 
     function addCircle(x, y) {
-
-        if (circles.length > trailLimit) {
-            circles[0].remove();
-            circles.shift();
-        }
-        circles.push(canvas.circle(x, y, 3));
+        circles.push(canvas.circle(x, y, 3).attr({
+            stroke: 'black',
+            strokeWidth: strokeWidth
+        }));
     }
 
+    function addRect(x, y) {
+        canvas.rect(x, y, 10, 10).attr({ stroke: color });
+    }
+
+    function addLine(mouseX, mouseY) {
+
+        // var targetX = mouseX;
+        // x += (targetX - x) * easing;
+        // var targetY = mouseY;
+        // y += (targetY - y) * easing;
+        // var weight = Snap.len(x, y, px, py)
+
+        // canvas.line(x, y, px, py).attr({
+        //     stroke: 'black',
+        //     strokeWidth: weight
+        // });
+        // py = y;
+        // px = x;
+
+        var lastMouse = mousePositions[mousePositions.length - 3];
+
+        if (lastMouse) {
+            elements.push(canvas.line(lastMouse.x, lastMouse.y, mouseX, mouseY).attr({
+                stroke: color,
+            }));
+        }
+
+    }
 };
+
+function undo() {
+    if (elements.length > 0) {
+        elements[elements.length - 1].remove();
+        elements.pop();
+    }
+}
+function clearAll() {
+    elements.forEach(element => {
+        element.remove();
+    });
+    elements = [];
+}
 
